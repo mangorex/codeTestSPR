8b8bae81ed8dae3b9f2da8b838ad9dabfa58767d
mangorex <mangorex89@gmail.com>
2022-08-21 17:54:08 +0200

    Refactor: Delete Score class

--- FWCScoreBoardTDD/FWCUnitTest.cs
+++ FWCScoreBoardTDD/FWCUnitTest.cs
@@ -14,8 +14,8 @@ namespace FWCScoreBoardTDD
         {
             DateTime dt = DateTime.Now;
             Match m = new Match("Mexico", "Canada", dt);
-            Assert.Equal(0, m.Score.HomeScore);
-            Assert.Equal(0, m.Score.AwayScore);
+            Assert.Equal(0, m.HomeScore);
+            Assert.Equal(0, m.AwayScore);
             Assert.True(m.Date.Equals(dt));
             FootballWorldCup.AddMatch(m);
             Assert.True(FootballWorldCup.CheckIfMatchExist(m));
@@ -39,8 +39,8 @@ namespace FWCScoreBoardTDD
             Match m = new Match("Mexico", "Canada", dt);
             FootballWorldCup.AddMatch(m);
             FootballWorldCup.UpdateScore(m.Id, 2, 3);
-            Assert.Equal(2, m.Score.HomeScore);
-            Assert.Equal(3, m.Score.AwayScore);
+            Assert.Equal(2, m.HomeScore);
+            Assert.Equal(3, m.AwayScore);
         }
 
         [Fact]
@@ -63,28 +63,28 @@ namespace FWCScoreBoardTDD
             FootballWorldCup.AddMatch(m);
             FootballWorldCup.UpdateScore(m.Id, 3, 1);
 
-            ConcurrentBag<Match> scoreBoardSummary = FootballWorldCup.GetSummaryGames();
+            OrderedParallelQuery<Match> scoreBoardSummary = FootballWorldCup.GetSummaryGames();
             Assert.True(
                 scoreBoardSummary.ElementAt(0).HomeTeam.Equals("Uruguay") &&
                 scoreBoardSummary.ElementAt(0).AwayTeam.Equals("Italy") &&
-                scoreBoardSummary.ElementAt(0).Score.HomeScore == 6 &&
-                scoreBoardSummary.ElementAt(0).Score.AwayScore == 6 &&
+                scoreBoardSummary.ElementAt(0).HomeScore == 6 &&
+                scoreBoardSummary.ElementAt(0).AwayScore == 6 &&
                 scoreBoardSummary.ElementAt(1).HomeTeam.Equals("Spain") &&
                 scoreBoardSummary.ElementAt(1).AwayTeam.Equals("Brazil") &&
-                scoreBoardSummary.ElementAt(1).Score.HomeScore == 10 &&
-                scoreBoardSummary.ElementAt(1).Score.AwayScore == 2 &&
+                scoreBoardSummary.ElementAt(1).HomeScore == 10 &&
+                scoreBoardSummary.ElementAt(1).AwayScore == 2 &&
                 scoreBoardSummary.ElementAt(2).HomeTeam.Equals("Mexico") &&
                 scoreBoardSummary.ElementAt(2).AwayTeam.Equals("Canada") &&
-                scoreBoardSummary.ElementAt(2).Score.HomeScore == 6 &&
-                scoreBoardSummary.ElementAt(2).Score.AwayScore == 6 &&
+                scoreBoardSummary.ElementAt(2).HomeScore == 6 &&
+                scoreBoardSummary.ElementAt(2).AwayScore == 6 &&
                 scoreBoardSummary.ElementAt(3).HomeTeam.Equals("Argentina") &&
                 scoreBoardSummary.ElementAt(3).AwayTeam.Equals("Australia") &&
-                scoreBoardSummary.ElementAt(3).Score.HomeScore == 3 &&
-                scoreBoardSummary.ElementAt(3).Score.AwayScore == 1 &&
+                scoreBoardSummary.ElementAt(3).HomeScore == 3 &&
+                scoreBoardSummary.ElementAt(3).AwayScore == 1 &&
                 scoreBoardSummary.ElementAt(4).HomeTeam.Equals("Germany") &&
                 scoreBoardSummary.ElementAt(4).AwayTeam.Equals("France") &&
-                scoreBoardSummary.ElementAt(4).Score.HomeScore == 2 &&
-                scoreBoardSummary.ElementAt(4).Score.AwayScore == 2
+                scoreBoardSummary.ElementAt(4).HomeScore == 2 &&
+                scoreBoardSummary.ElementAt(4).AwayScore == 2
             );
 
         }
